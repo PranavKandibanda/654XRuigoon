e0a905f702dd3469c3031a3f475ad09c7796e51b
Pranav K <pkandibanda123@gmail.com>
2026-03-07 18:42:52 -0600

    skills auto pt.1 (first loader and long goal)

---
+++ src/main.cpp
@@ -0,0 +1,421 @@
+#include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/adi.hpp"
+#include "pros/device.hpp"
+#include "pros/distance.hpp"
+#include "pros/misc.h"
+#include "pros/rtos.hpp"
+#include "pros/rotation.hpp"
+#include "pros/imu.hpp"
+#include "pros/distance.hpp"
+
+// controller
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+// motor groups
+pros::MotorGroup leftMotors({-2, -3, -4},
+                            pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
+pros::MotorGroup rightMotors({7, 9, 8}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
+
+pros::adi::Pneumatics loading_piston('A',false);
+pros::adi::Pneumatics scoring_piston('B',false);
+pros::adi::Pneumatics scraper_piston('D',false);
+
+pros::MotorGroup intake ({1,-5}, pros::MotorGearset::blue);
+
+void load()
+{
+    loading_piston.set_value(true);
+    scoring_piston.set_value(true);
+    intake.move(127);
+}
+
+void middle_score()
+{
+    loading_piston.set_value(false);
+    scoring_piston.set_value(true);
+    intake.move(127);
+}
+
+void score()
+{
+    loading_piston.set_value(true);
+    scoring_piston.set_value(false);
+    intake.move(127);
+}
+
+void reverse()
+{
+    loading_piston.set_value(true);
+    scoring_piston.set_value(false);
+    intake.move(-127);
+}
+
+
+
+// Inertial Sensor on port 10
+pros::Imu imu(12);
+
+double front_sensor_offset = 8.5;
+double left_sensor_offset = 5.5;
+double right_sensor_offset = 5.5;
+double back_sensor_offset = 6;
+double field_half_size = 72; // Half of the field size in inches
+
+pros::Distance front_sensor(6);
+pros::Distance right_sensor(20);
+pros::Distance left_sensor(16);
+pros::Distance back_sensor(11);
+
+
+
+// tracking wheels
+// horizontal tracking wheel encoder. Rotation sensor, port 20, not reversed
+pros::Rotation horizontalEnc(20);
+// vertical tracking wheel encoder. Rotation sensor, port 11, reversed
+pros::Rotation verticalEnc(-11);
+// horizontal tracking wheel. 2.75" diameter, 5.75" offset, back of the robot (negative)
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -5.75);
+// vertical tracking wheel. 2.75" diameter, 2.5" offset, left of the robot (negative)
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_275, -2.5);
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
+                              &rightMotors, // right motor group
+                              11, // 10 inch track width
+                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                              450, // drivetrain rpm is 360
+                              8 // horizontal drift is 2. If we had traction wheels, it would have been 8
+);
+
+// lateral motion controller
+lemlib::ControllerSettings linearController(8, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            9, // derivative gain (kD)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            15 // maximum acceleration (slew)
+);
+
+// angular motion controller
+lemlib::ControllerSettings angularController(1.7, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             14.5, // derivative gain (kD)
+                                             3, // anti windup
+                                             4, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             6, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+// sensors for odometry
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
+                            nullptr, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors, &throttleCurve, &steerCurve);
+
+
+void resetPositionWithSensor(pros::Distance& sensor, double sensor_offset, double sensor_angle_offset, double field_half_size) {
+    double sensorReading = sensor.get_distance()*0.0393701; //mm to inches
+  
+    // Check for invalid reading (distance sensors return -1 or very large values when no object detected)
+    if (sensorReading < 0 || sensorReading > 200) {
+        pros::lcd::print(0,"invalid sensor reading"); // x
+        return;
+    }
+    
+    // Get current pose
+    double current_heading_deg = chassis.getPose().theta;
+    double robot_heading_deg = current_heading_deg + sensor_angle_offset;
+    
+    // Normalize heading to 0-360 range
+    int headingDeg = (int)(robot_heading_deg);
+    headingDeg = (headingDeg + 360) % 360;
+    
+    // Determine which wall we're facing and which axis to reset
+    bool resettingX = false;
+    double wallSign = 1.0;
+    
+    if (315 <= headingDeg || headingDeg <= 45) {
+        // Top wall - reset Y position
+        resettingX = false;
+        wallSign = 1.0;
+    }
+    else if (45 < headingDeg && headingDeg <= 135) {
+        // Right wall - reset X position
+        resettingX = true;
+        wallSign = 1.0;
+    }
+    else if (135 < headingDeg && headingDeg <= 225) {
+        // Bottom wall - reset Y position
+        resettingX = false;
+        wallSign = -1.0;
+    }
+    else {
+        // Left wall - reset X position
+        resettingX = true;
+        wallSign = -1.0;
+    }
+    
+    // Calculate distance from wall to robot center
+    double wallToCenter = sensorReading + sensor_offset;
+    
+    // Calculate actual position
+    double actualPos = wallSign * (field_half_size - wallToCenter);
+    
+    // Update position (only reset the appropriate axis)
+    if (resettingX) {
+        chassis.setPose(actualPos, chassis.getPose().y, chassis.getPose().theta);
+
+    } else {
+        chassis.setPose(chassis.getPose().x, actualPos, chassis.getPose().theta);
+    }
+}
+
+/*
+ * resetPositionFront
+ * Resets position using the front distance sensor.
+ * Remember to only use these when perpendicular to the wall!
+ * - sensor: Front distance sensor
+ * - sensor_offset: Distance offset of the sensor from robot center (in inches)
+ * - field_half_size: Half the field dimension (distance from center to wall, in inches)
+ */
+void resetPositionFront() {
+    resetPositionWithSensor(front_sensor, front_sensor_offset, 0.0, field_half_size);
+}
+
+/*
+ * resetPositionBack
+ * Resets position using the back distance sensor.
+ * Remember to only use these when perpendicular to the wall!
+ * - sensor: Back distance sensor 
+ * - sensor_offset: Distance offset of the sensor from robot center (in inches)
+ * - field_half_size: Half the field dimension (distance from center to wall, in inches)
+ */
+void resetPositionBack() {
+    resetPositionWithSensor(back_sensor, back_sensor_offset, 180.0, field_half_size);
+}
+
+/*
+ * resetPositionLeft
+ * Resets position using the left distance sensor.
+ * Remember to only use these when perpendicular to the wall!
+ * - sensor: Left distance sensor
+ * - sensor_offset: Distance offset of the sensor from robot center (in inches)
+ * - field_half_size: Half the field dimension (distance from center to wall, in inches)
+ */
+void resetPositionLeft() {
+    resetPositionWithSensor(left_sensor, left_sensor_offset, 270.0, field_half_size);
+}
+
+/*
+ * resetPositionRight
+ * Resets position using the right distance sensor.
+ * Remember to only use these when perpendicular to the wall!
+ * - sensor: Right distance sensor
+ * - sensor_offset: Distance offset of the sensor from robot center (in inches)
+ * - field_half_size: Half the field dimension (distance from center to wall, in inches)
+ */
+void resetPositionRight() {
+    resetPositionWithSensor(right_sensor, right_sensor_offset, 90.0, field_half_size);
+}
+
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+
+    // the default rate is 50. however, if you need to change the rate, you
+    // can do the following.
+    // lemlib::bufferedStdout().setRate(...);
+    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+
+    // for more information on how the formatting for the loggers
+    // works, refer to the fmtlib docs
+
+    // thread to for brain screen and position logging
+    pros::Task screenTask([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // log position telemetry
+            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            // delay to save resources
+            pros::delay(50);
+        }
+    });
+}
+
+/**
+ * Runs while the robot is disabled
+ */
+void disabled() {}
+
+/**
+ * runs after initialize if the robot is connected to field control
+ */
+void competition_initialize() {}
+
+// get a path used for pure pursuit
+// this needs to be put outside a function
+
+/**
+ * Runs during auto
+ *
+ * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
+ */
+void autonomous() {
+    chassis.setPose(-44.457,7,90);
+    resetPositionBack();
+
+    //getting four balls in the center
+    load();
+    chassis.swingToHeading(45, DriveSide::LEFT, 500,{},false);
+    chassis.moveToPoint(-24, 24, 1000,{.maxSpeed=87}, false);
+    //getting four balls in the center
+
+    
+    //scoring middle
+    chassis.turnToHeading(315, 2000,{}, false);
+    chassis.moveToPoint(-12,12, 2000,{.forwards=false}, false);
+    middle_score();
+    pros::delay(500);
+    //scoring middle
+
+    //going to loader
+    chassis.moveToPoint(-48, 48, 1000,{.maxSpeed = 97}, false);
+    chassis.turnToHeading(266, 1000,{}, false);
+    resetPositionRight();
+    resetPositionFront();
+    load();
+
+    chassis.moveToPoint(-35,48,500,{.forwards=false},false);
+    score();
+    pros::delay(500);
+
+    scraper_piston.set_value(true);
+    chassis.moveToPoint(-58.5, chassis.getPose().y, 2000,{},false);
+    pros::delay(500);
+    //going to loader
+
+
+    //exiting loader
+    chassis.moveToPoint(-48, chassis.getPose().y, 2000,{.forwards=false},false);
+    resetPositionRight();
+    resetPositionFront();
+    //exiting loader
+
+    //going to the side of first long goal
+    chassis.moveToPose(-36, 58,46+180,2000,{.forwards=false},false);
+    chassis.turnToHeading(270, 1000,{},false);
+    scraper_piston.set_value(false);
+    resetPositionRight();
+    resetPositionFront();
+    //going to the side of first long goal
+
+    //traversing the side of the first long goal
+    chassis.moveToPoint(36, chassis.getPose().y, 2000,{.forwards=false,.maxSpeed = 87},false);
+    resetPositionRight();
+    resetPositionFront();
+    //traversing the side of the first long goal
+
+    //aligning with the end of the first long goal
+    chassis.moveToPose(36, 48, 311, 2000,{.forwards=false},false);
+    chassis.turnToHeading(90, 2000,{},false);
+    //aligning with the end of the first long goal
+
+    //going to score on the first long goal
+    chassis.moveToPoint(chassis.getPose().x-9, chassis.getPose().y, 1000,{.forwards = false},false);
+    score();
+    pros::delay(1000);
+    resetPositionFront();
+    resetPositionLeft();
+    //going to score on the first long goal
+
+    //going to second loader
+    scraper_piston.set_value(true);
+    chassis.moveToPoint(58.5, chassis.getPose().y, 2000,{},false);
+    pros::delay(500);
+    //going to second loader
+
+    //scoring all of those blocks
+    chassis.moveToPoint(35, 48, 2000,{.forwards= false},false);
+    score();
+    pros::delay(1000);
+    resetPositionFront();
+    resetPositionLeft();
+    //scoring all of those blocks
+
+}
+
+
+/**
+ * Runs in driver control
+ */
+void opcontrol() {
+    // controller
+    // loop to continuously update motors
+	autonomous();
+    while (true) {
+        // get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
+        {
+            score();
+        }
+        else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+        {
+            reverse();
+        }
+        else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+        {
+            load();
+        }
+        else if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+        {
+            middle_score();
+        }
+        else
+        {
+            intake.move(0);
+        }
+        // move the chassis with curvature drive
+        chassis.arcade(leftY, rightX);
+        // delay to save resources
+        pros::delay(10);
+    }
+}
